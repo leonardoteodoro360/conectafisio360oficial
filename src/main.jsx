bd75d6c043588221a3ba6daebddcd67cc7a89dfc
leonardoteodoro360 <leonardo.teodoro@aluno.unifenas.br>
2026-02-20 10:48:30 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
-// src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import AuthProvider from "./contextos/AuthContext";
+import AuthProvider from "./contextos/AuthContext.jsx"; // ← extensão adicionada
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
